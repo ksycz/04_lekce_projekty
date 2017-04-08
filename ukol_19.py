d0f1952e80022489ca621fc1703ae703e23dcfe4
Katarzyna Sycz <katarzynasycz@gmail.com>
2017-04-09 00:49:48 +0200

    ukol_19

---
+++ ukol_19.py
@@ -0,0 +1,9 @@
+cislo1=int(input())
+cislo2=int(input())
+cislo3=int(input())
+if cislo1+cislo2+cislo3>10:
+    print('Soucet cisel je vetsi nez 10')
+elif cislo1+cislo2+cislo3==10:
+    print('Soucet cisel je 10')
+else:
+    print('Soucet cisel je mensi nez 10')
